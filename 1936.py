a53a21406d990f5f7aadedd7dfce43bf6a398c7c
JuHyun-E <47977872+JuHyun-E@users.noreply.github.com>
2020-03-26 14:05:06 +0900

    Update 1936.py

--- 1936.py
+++ 1936.py
@@ -8,16 +8,3 @@ if (A == 1 and B == 3) or (A == 2 and B == 1) or (A == 3 and B == 2):
     print('A')
 else:
     print('B')
-
-# 다른 사람이 한 것??? 이해못함
-'''
-s = input().split(" ")
-A=int(s[0])
-B=int(s[1])
-if (A+1)%3 == (B%3):  # 2, 3, 4 -> 2, 0, 1 == 1, 2, 0
-    print("B")
-elif (B+1)%3 == A%3:
-    print("A")
-else:
-    print("무승부")
-'''
